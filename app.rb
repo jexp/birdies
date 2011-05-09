26a304a13b62a6f43cf89ff4346fc5bdcacff64c
Michael Hunger <github@jexp.de>
2011-05-09 14:08:34 +0200

    temporarily removed user/tags output

--- app.rb
+++ app.rb
@@ -14,6 +14,8 @@ class App < Sinatra::Base
   end
 
   get '/' do
+    @users = []
+    @used_tags = []
 #    @users = @birds.users.collect{ |u| [u.twid, u.outgoing(:TWEETED).size] }.sort { |a,b| b[1] <=> a[1] }
 #    @used_tags = @birds.tags.collect{ |t| [t.name, t.incoming(:TAGGED).size] }.sort { |a,b| b[1] <=> a[1] }
     erb :index
