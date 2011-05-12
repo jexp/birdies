764745d237ee0b9e18b7bf7bd26bd9e4043fda20
Michael Hunger <github@jexp.de>
2011-05-12 13:19:37 +0200

    protovis weirdness with safari + chrome

--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,3 @@
-function render(data,canvas) {
-
-	var classes = pv.nodes(data);
-
-	var format = pv.Format.number();
-
-	var vis = new pv.Panel()
-//	.width(document.body.clientWidth)
-//	.height(document.body.clientHeight)
-//	.event("mousedown", pv.Behavior.pan())
-	.event("mousewheel", pv.Behavior.zoom())
-	.canvas(canvas);
-
- // document.location.href=
-
-	var pack = vis.add(pv.Layout.Pack)
-	.top(-50)
-	.bottom(-50)
-	.nodes(classes)
-	.size(function(d) (d.nodeValue.value+1)*1000)
-	.spacing(0)
-	.order(null)
-	.node.add(pv.Dot)
-	.event("click", function(d) self.location.href=d.nodeValue.link)
-//	.event("mouseover", function() this.fillStyle("orange"))
-//	.event("mouseout", function() this.fillStyle(undefined))
-	.fillStyle(pv.Colors.category19().by(function(d) d.nodeValue.name))
-	.strokeStyle(function() this.fillStyle().darker())
-	.visible(function(d) d.parentNode)
-	.title(function(d) d.nodeValue.name + ": " + format(d.nodeValue.value))
-	.anchor("center").add(pv.Label)
-	.text(function(d) d.nodeValue.name.substring(0, d.nodeValue.value*4))
-	;
-
-	vis.render();
-}
 
 function graph(data,canvas) {
 
